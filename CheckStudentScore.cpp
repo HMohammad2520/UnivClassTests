b07746b8d7bb9bbf613e865f7565e3a93d9ad371
Mohammad Heydari <HMohammad2520@Gmail.com>
2024-11-04 22:16:18 +0330

    Pretified

--- CheckStudentScore.cpp
+++ CheckStudentScore.cpp
@@ -1,34 +1,41 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int Score;
 
     cout << "Nomre Danesh Amoz ra Vared Konid: ";
     cin >> Score;
 
-    if (20 >= Score && Score > 18){
+    if (20 >= Score && Score > 18)
+    {
         cout << "Kheyli Khob !!";
         return 0;
     }
-    else if (18 >= Score && Score > 16) {
+    else if (18 >= Score && Score > 16)
+    {
         cout << "Khob";
         return 0;
     }
-    else if (16 >= Score && Score > 14) {
+    else if (16 >= Score && Score > 14)
+    {
         cout << "Ghabel Ghabol";
         return 0;
     }
-    else if (14 >= Score && Score > 12) {
+    else if (14 >= Score && Score > 12)
+    {
         cout << "Niyaz Be Talash Bishtar";
         return 0;
     }
-    else if (12 >= Score && Score > 0) {
+    else if (12 >= Score && Score > 0)
+    {
         cout << "Mardood";
         return 0;
     }
-    else {
+    else
+    {
         cout << "## Nomre Eshtebah Bood !!";
         return 1;
     }
